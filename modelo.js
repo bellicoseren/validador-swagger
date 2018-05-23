1ae463be28f1fd06778cf2adb4cb07e39539a26f
dianalml <dianalmtzleon@gmail.com>
2018-05-23 10:24:56 -0500

    Se actualiza el servicio consultaBitacoraOperaciones

--- modelo.js
+++ modelo.js
@@ -4281,20 +4281,20 @@ const consultaBitacoraOperaciones = {
     {
       "idBitacora": 1234567,
       "folio": "F0123UNASN3123",
-      "fechaSolicitud": "21-04-2017 11:03:00",
-      "fechaProgramada": "",
+      "fechaSolicitud": "21-04-2017T11:03:00",
+      "fechaProgramada": "22-04-2017T11:03:00",
       "importe": 37192.12,
       "moneda": "MXN",
       "estatus": "APLICADA",
       "folio2": "F0123UNASN3123",
-      "fechaAutorizacion": "21-04-2017 11:03:00",
+      "fechaAutorizacion": "21-04-2018T11:03:00",
       "folioAutorizacion": "FO2389JDS912",
       "medio": "MultivaNet",
       "concepto": "Pago de servicios financieros",
       "referencia": "128362518293",
       "claveRastreo": "1729HSQA9383712JD",
-      "fechaAplicacion": "14-10-2017",
-      "fechaLiquidacion": "14-10-2017",
+      "fechaAplicacion": "14-10-2017T17:47:00",
+      "fechaLiquidacion": "14-10-2017T17:47:00",
       "idDispositivo": "AJDH1023731KDAD",
       "agente": "Jorge Posadas",
       "rfc": "ABCD123456ASD",
@@ -4500,8 +4500,7 @@ var mapaModelo = {
  'consultaLimitesUsuario':consultaLimitesUsuario,
  'resetPasswordUsuario': resetPasswordUsuario,
  'consultaEmpresasPorHolding': consultaEmpresasPorHolding, 
- 'consultaBitacoraOperaciones': consultaBitacoraOperaciones,
- 'consultaEmpresasPorHolding': consultaEmpresasPorHolding
+ 'consultaBitacoraOperaciones': consultaBitacoraOperaciones
 
 
 }
